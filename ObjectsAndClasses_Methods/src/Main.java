2703776268bcac5dabc8d2073da6edbc3cf5dd89
Спартак Исхаков <spartak10182@yandex.ru>
2022-11-13 23:48:19 +0300

    Arithmetic add

---
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.add("Milk", 40);
+        basket.print("Milk");
+
+
+        Basket basket1 = new Basket();
+        basket1.add("Сосиски", 670, 3, 0.9);
+        basket1.add("Мангал", 4000);
+        basket1.add("Квас", 150, 5, 1.5);
+        basket1.add("Лаваш", 90, 3, 0.45);
+        basket1.print("Корзина");
+    }
+}

e91dfe648e87bca03fd25a8c5d672e549e4d59f3
Спартак Исхаков <spartak10182@yandex.ru>
2022-11-20 00:14:27 +0300

    Practical work 6.8

--- ObjectsAndClasses_Encapsulation/src/Main.java
+++ ObjectsAndClasses_Encapsulation/src/Main.java
@@ -14,13 +14,16 @@ public class Main {
                 1500, "Морская 15", true, "sdf3153121", true);
         System.out.println(original);
         Order newOrder = original.setDeliveryAddress("Туманная 2");
+        newOrder = newOrder.setRegistrationNumber("SXCDFV1234");
+        newOrder = newOrder.setFragile(false);
+        newOrder = newOrder.setDimensionsLong(2.2);
         System.out.println(newOrder);
-        original.setDimensionsWidth(1.5);
+/*        original.setDimensionsWidth(1.5);
         original.setRegistrationNumber("ASDFQW98098");
         original.setMass(2500);
         original.print();
         System.out.println(original);
-        System.out.println(newOrder);
+        System.out.println(newOrder);*/
 
     }
 }

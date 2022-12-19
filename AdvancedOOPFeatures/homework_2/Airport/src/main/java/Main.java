6a9f681650ef8dd014e008aba1dc6d9902ef5966
Спартак Исхаков <spartak10182@yandex.ru>
2022-12-20 02:12:05 +0300

    complete 13.14

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,7 +1,13 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+import com.skillbox.airport.Terminal;
+import net.sf.saxon.functions.AccessorFn;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +17,29 @@ public class Main {
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        Date date = new Date();
+        Long dateLong = date.getTime() + 7200000;
+        Date date1 = new Date(dateLong);
+        return airport
+                .getTerminals()
+                .stream()
+                .flatMap(
+                        terminal -> terminal
+                                .getFlights()
+                                .stream()
+                                .filter(
+                                        flight -> flight
+                                                .getDate()
+                                                .getTime() > date.getTime() && flight
+                                                .getDate()
+                                                .getTime() < date1.getTime()
+                                )
+                                .filter(
+                                        flight -> flight
+                                                .getType()
+                                                .equals(Flight.Type.DEPARTURE))
+                )
+                .collect(Collectors.toList()
+                );
     }
-
 }

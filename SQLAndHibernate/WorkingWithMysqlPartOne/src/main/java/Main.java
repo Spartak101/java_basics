9c0bc5a3c96be0b86f17a6d7655517129b8becbc
Спартак Исхаков <spartak10182@yandex.ru>
2023-01-30 14:04:14 +0300

    Month in Year OK

--- SQLAndHibernate/WorkingWithMysqlPartOne/src/main/java/Main.java
+++ SQLAndHibernate/WorkingWithMysqlPartOne/src/main/java/Main.java
@@ -16,17 +16,14 @@ public class Main {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT course_name, (q.c_course / c.c_all) AS effect\n" +
-                    "FROM (SELECT DISTINCT course_name, MONTH(subscription_date) AS c_course\n" +
-                    "FROM skillbox.purchaselist\n" +
-                    "WHERE subscription_date IN (\n" +
-                    "SELECT max(subscription_date)\n" +
-                    "FROM skillbox.purchaselist\n" +
-                    "GROUP BY course_name)) AS q\n" +
-                    "JOIN\n" +
-                    "(SELECT DISTINCT course_name AS cn, count(subscription_date) AS c_all\n" +
-                    "FROM skillbox.purchaselist\n" +
-                    "GROUP BY course_name) AS c\n" +
-                    "ON q.course_name = c.cn");
+                    "                    FROM (SELECT course_name, (max(MONTH(subscription_date)) - MIN(MONTH(subscription_date))) AS c_course\n" +
+                    "                    FROM skillbox.purchaselist\n" +
+                    "                    GROUP BY course_name) AS q\n" +
+                    "                    JOIN\n" +
+                    "                    (SELECT DISTINCT course_name AS cn, count(subscription_date) AS c_all\n" +
+                    "                    FROM skillbox.purchaselist\n" +
+                    "                    GROUP BY course_name) AS c\n" +
+                    "                    ON q.course_name = c.cn");
             while (resultSet.next()){
 
                 String courseName = resultSet.getString("course_name");
